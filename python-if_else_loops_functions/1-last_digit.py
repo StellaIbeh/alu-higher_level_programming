f2c42d86b5bafc907ced4522a33d8bd212f171fb
StellaIbeh <a.ibeh@alustudent.comgit config --global user.email a.ibeh@alustudent.com>
2023-02-07 14:54:38 -0800

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
     last = -last
     print("Last digit of {} is {} and is ".format(number, last), end="")
 if last > 5:
-     print("Last digit of", number, "is", last, "and is greater than 5")
+     print("greater than 5")
 elif last == 0:
-    print("Last digit of", number, "is", last, "and is 0")
+    print("0")
 elif last < 6:
     print("less than 6 and not 0")
