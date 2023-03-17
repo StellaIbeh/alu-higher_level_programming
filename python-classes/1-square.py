e29c3f972354df5fe0f615299a2a7735ffe591fe
StellaIbeh <a.ibeh@alustudent.comgit config --global user.email a.ibeh@alustudent.com>
2023-03-17 01:12:07 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-'Write a class Square that defines a square by'
+
+
+"""
+Write a class Square that defines a square
+with private fields
+"""
 
 
 class Square:
-    'Private instance attribute: size
-    Instantiation with size (no type/value verification)'
+    """
+    This class takes creates a square
+    """
+
     def __init__(self, size):
+        """
+        Pass in the size of the square
+        """
         self.__size = size
