1b19bda2c35d16e83fec3c486ca286872e6b5033
StellaIbeh <a.ibeh@alustudent.com>
2023-06-13 11:26:19 +0000

    tasks

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const argument = process.argv[2];
-if (argument === undefined) {
+const arg  = process.argv[2];
+if (arg == null {
   console.log('No argument');
 } else {
-  console.log(argument);
+  console.log(arg);
