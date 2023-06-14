135f9a6a5fef03e16c9c65c7f3aa7e54f59debbd
StellaIbeh <a.ibeh@alustudent.com>
2023-06-14 05:46:10 +0000

    multi_c

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const argNum = Number (argv[2]);
+if (argNum) {
+  for (let a = 0; a < argNum; a++) {
+console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
