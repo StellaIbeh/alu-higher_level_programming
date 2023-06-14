f65db02bb2c8534475c694b89af04373a5171e86
StellaIbeh <a.ibeh@alustudent.com>
2023-06-14 08:49:12 +0000

    fix

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,12 @@
-#!/usr/bin/node    
+#!/usr/bin/node
+
 const argNum = Number(process.argv[2]);
+
 if (!Number.isNaN(argNum)) {
- for (let i = 0; i < argNum; i++) {
-  console.log('C is fun');
+  for (let i = 0; i < argNum; i++) {
+    console.log('C is fun');
   }
 } else {
- console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
+
