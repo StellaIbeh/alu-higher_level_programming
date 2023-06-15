47e574025da4b1fb54fae174a7cac8fbd7d567dd
StellaIbeh <a.ibeh@alustudent.com>
2023-06-15 01:32:18 +0000

    8-esrever.js fix

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 exports.esrever = function (list) {
   const length = list.length;
   const newList = [];
-  for (let i = length - 1; i> =0; i--) {
-    newList.push(list[i]);
-  }
-  return newList;
+  for (let i = length - 1; i >= 0; i--) {
+        newList.push(list[i]);
+    }
+    return newList;
 };
