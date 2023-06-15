ef3e15bb102da63ffb7b352f18aecdfc2e8433a2
StellaIbeh <a.ibeh@alustudent.com>
2023-06-15 02:42:44 +0000

    101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,46 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const { argv, exit } = require('process');
 
-if (argv.length !== 3) {
-  exit(0);
-}
-
-const options = {
-  url: `https://swapi-api.hbtn.io/api/films/${argv[2]}/`,
-  method: 'GET',
-  headers: {
-    'Accept-Charset': 'utf-8'
+function helpRequest (arr, i) {
+  if (i === arr.length) {
+    return;
   }
-};
-
-function requestStart (optionsUrl, saveValue) {
-  request(optionsUrl, function (err, res, body) {
-    if (err) throw err;
-
-    const characters = JSON.parse(body).characters;
-    const result = {};
-
-    characters.forEach(element => {
-      request(element, function (err, res, _body) {
-        if (err) throw err;
-
-        const name = JSON.parse(_body).name;
-
-        result[element.split('/')[5]] = name;
-        saveValue(result, characters.length);
-      });
-    });
+  request(arr[i], function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(JSON.parse(body).name);
+    helpRequest(arr, i + 1);
   });
 }
 
-let i = 0;
-requestStart(options, function (dict, length) {
-  i++;
-  if (i === length) {
-    for (const name in dict) {
-      console.log(dict[name]);
-    }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
+  const charac = JSON.parse(body).characters;
+  helpRequest(charac, 0);
 });
