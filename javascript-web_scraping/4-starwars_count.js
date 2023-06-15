a1a8370803089f440d63c77eb0dc76554dbd8192
StellaIbeh <a.ibeh@alustudent.com>
2023-06-15 02:33:44 +0000

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
-const req = require('request');
-const url = process.argv[2];
 
-let count = 0;
-let data;
-req.get(url, (err, res) => {
-  if (err) console.log(err);
-  else {
-    data = JSON.parse(res.body).results;
-    data.forEach((obj) => {
-      obj.characters.forEach((character) => {
-        if (character.includes('/18/')) count++;
-      });
-    });
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
-  console.log(count);
+  /* const nb = JSON.parse(body).results.reduce((acc, elem) => {
+    acc += elem.characters.reduce((acc, character) => {
+      return (character === 'https://swapi.co/api/people/18/' ? acc + 1 : acc);
+    }, 0);
+    return (acc);
+  }, 0);
+  */
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.filter((url) => { return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
 });
